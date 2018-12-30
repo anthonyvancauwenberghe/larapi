9a3a2ef8440fb05f62f5767c4080e777a5b32182
arthur <aamining2@gmail.com>
2018-12-30 22:30:34 +0100

    cache speed test

--- src/Foundation/Tests/CacheObserverTest.php
+++ src/Foundation/Tests/CacheObserverTest.php
@@ -4,6 +4,7 @@ namespace Foundation\Tests;
 
 use Foundation\Abstracts\Tests\TestCase;
 use Foundation\Cache\ModelCache;
+use Modules\Account\Entities\Account;
 
 class CacheObserverTest extends TestCase
 {
@@ -14,6 +15,30 @@ class CacheObserverTest extends TestCase
         unset($user->role_ids);
         $this->assertEquals($user->toArray(), $user::cache()->find($user->id, false)->toArray());
         $this->assertEquals($user->toArray(), $user::cache()->findBy('identity_id', $user->identity_id, false)->toArray());
+    }
+
+    public function testCacheSpeed(){
+        $model = Account::create(["testthisshit"=>5]);
+        \Cache::put('testmodel',$model);
+
+        $time_db_start = microtime(true);
+        for($i=0; $i<1000;$i++){
+            Account::find($model->id);
+        }
+        $time_db_end = microtime(true);
+
+        $dbTime = $time_db_end - $time_db_start;
+
+        $time_cache_start = microtime(true);
+
+        for($i=0; $i<1000;$i++){
+            \Cache::get('testmodel');
+        }
+        $time_cache_end = microtime(true);
+
+        $cacheTime = $time_cache_end - $time_cache_start;
+
+        $this->assertGreaterThan($cacheTime, $dbTime);
     }
 
     public function testClearModelsCache()
